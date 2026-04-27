1dbdb582b8330f08271ecec6746583e01f56becb
Stefania Silva <stefaniasilva0000@gmail.com>
2026-04-27 17:03:44 -0300

    feat: implementa chave por palavra Vigenére-like

--- chave.c.py
+++ chave.c.py
@@ -2,16 +2,24 @@ import string
 
 alfabeto = string.ascii_uppercase + string.digits + string.punctuation + ' '
 texto = input('Digite a mensagem: ').upper()
-chave = 5
+chave_palavra = input('Digite a chave:').upper()
 mensagem_cifrada = ''
-for letra in texto:
+chave_numeros = []
+for letra in chave_palavra:
+    chave_numeros.append(alfabeto.index(letra))
+for i, letra in enumerate(texto):
     indice = alfabeto.index(letra)
-    novo_indice = (indice+chave) % 26
-    nova_letra = alfabeto[ novo_indice ]
+    posicao = chave_numeros[i % len(chave_numeros)]
+    novo_indice = (indice + posicao) % len (alfabeto)
+    nova_letra = alfabeto[novo_indice]
     mensagem_cifrada += nova_letra
     print(nova_letra)
 with open ('mensagem_cifrada.txt', 'w') as arquivo:
     arquivo.write(mensagem_cifrada)
+
+
+
+
 
 
 
